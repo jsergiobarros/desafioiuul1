a2a14aa3069eed0d1131aef222a8fa82f7a4a6d9
sergio <jsergiobarros@gmail.com>
2024-03-28 19:17:36 -0300

    mostrando imagens, precisa ajustar tamanho dos botões

--- js/scripts.js
+++ js/scripts.js
@@ -7,38 +7,52 @@ class Messages{
         this.email=email
         this.message=message
     }
+
+}
+function enableButton(){
+    if( document.getElementById("name").value!=="" &&
+        document.getElementById("email").value!=="" &&
+        document.getElementById("message").value!=="")
+        document.getElementById('sendMessage').disabled=false;
+    else
+        document.getElementById('sendMessage').disabled=true;
 }
 
 function listMessage(){
     let messageDiv = document.getElementById('message-list');
-    let messageList=localStorage.getItem('messages')
+    let messageList= localStorage.getItem('messages')
+    console.log(messageList)
     if(messageList===""|| messageList===null)
         messageDiv.innerHTML="<p>Sem mensagens no momento</p>"
     else{
         messageDiv.innerHTML=""
-        for (let i in messageList){
-            console.log(i)
-            messageDiv.innerHTML+= "<h3>${i.name}</h3>"
-        }
+        JSON.parse(messageList).map((i) => {
+            messageDiv.innerHTML+= '<h3 >'+i.name+'</h3>'+'<h4>'+i.email+'</h4>'+'<h5>'+i.message+'</h5>'
+
+        })
     }
 
 }
 
-function getMessage(name, email,message){
-    console.log(localStorage.getItem('messages'))
+function setMessage(){
+
     let messageList=localStorage.getItem('messages')===""|| localStorage.getItem('messages')===null? new Array(): JSON.parse( localStorage.getItem('messages'))
-    let form = new Messages(name.value,email.value,message.value)
+    let form = new Messages(
+        document.getElementById("name").value,
+        document.getElementById("email").value,
+        document.getElementById("message").value)
+
     messageList.push(form)
     localStorage.setItem("messages",JSON.stringify(messageList))
-    console.log(localStorage)
+
     document.getElementById("name").value=""
     document.getElementById("email").value=""
     document.getElementById("message").value=""
-    listMessage()
+    enableButton()
 
 }
 function clearForm(){
     localStorage.setItem("messages","")
-    console.log(localStorage)
+
 
 }
