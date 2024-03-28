c3c25d2bda8f577c4389b8c693dd3f3d12281571
sergio <jsergiobarros@gmail.com>
2024-03-28 14:50:47 -0300

    editando mostrar mensagens

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,24 @@ class Messages{
     }
 }
 
+function listMessage(){
+    let messageDiv = document.getElementById('message-list');
+    let messageList=localStorage.getItem('messages')
+    if(messageList===""|| messageList===null)
+        messageDiv.innerHTML="<p>Sem mensagens no momento</p>"
+    else{
+        messageDiv.innerHTML=""
+        for (let i in messageList){
+            console.log(i)
+            messageDiv.innerHTML+= "<h3>${i.name}</h3>"
+        }
+    }
+
+}
+
 function getMessage(name, email,message){
-    let messageList=localStorage.getItem('messages')===""? new Array(): JSON.parse( localStorage.getItem('messages'))
+    console.log(localStorage.getItem('messages'))
+    let messageList=localStorage.getItem('messages')===""|| localStorage.getItem('messages')===null? new Array(): JSON.parse( localStorage.getItem('messages'))
     let form = new Messages(name.value,email.value,message.value)
     messageList.push(form)
     localStorage.setItem("messages",JSON.stringify(messageList))
@@ -18,6 +34,7 @@ function getMessage(name, email,message){
     document.getElementById("name").value=""
     document.getElementById("email").value=""
     document.getElementById("message").value=""
+    listMessage()
 
 }
 function clearForm(){
